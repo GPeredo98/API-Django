3e9a8b00d3c3be1cc78dc770f6bcd87cfafc3785
Gabriel Peredo <gabriel.peredo777@gmail.com>
2021-01-10 11:55:47 -0400

    feat: Modelo del producto

--- productos/models.py
+++ productos/models.py
@@ -1,3 +1,27 @@
+from datetime import datetime
+
 from django.db import models
 
-# Create your models here.
+
+class Producto(models.Model):
+
+    class ProductoCategorias(models.IntegerChoices):
+        MODA = 1
+        DEPORTE = 2
+        SALUD = 3
+        BELLEZA = 4
+        HERRAMIENTAS = 5
+        TECNOLOGIA = 6
+
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField(null=True)
+    categoria = models.IntegerField(choices=ProductoCategorias.choices, default=ProductoCategorias.MODA)
+    precio = models.IntegerField(null=False)
+    cantidad = models.IntegerField(null=False)
+    disponible = models.BooleanField(default=True)
+    fecha_creacion = models.DateTimeField(default=datetime.now())
+    imagen = models.TextField(null=True)
+
+    class Meta:
+        db_table = 'productos'
